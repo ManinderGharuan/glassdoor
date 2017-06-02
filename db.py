bcfb0fa15382e1ac18d490112b029e65bd298292
Maninder Singh <maninderrai7@gmail.com>
2017-06-02 13:57:36 +0530

    Create models for database
    - scraper session and glassdoor session are in db file

---
+++ db.py
@@ -0,0 +1,32 @@
+from os import path
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+Scraper_Base = declarative_base()
+
+
+def get_session():
+    engine = create_engine('sqlite:///glassdoor.db')
+
+    if not path.exists('glassdoor.db'):
+        Base.metadata.create_all(engine)
+
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
+    return session
+
+
+def get_scraper_session():
+    engine = create_engine('sqlite:///scraper.db')
+
+    if not path.exists('scraper.db'):
+        Scraper_Base.metadata.create_all(engine)
+
+    Scraper_Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+
+    return DBSession()
---
+++ models.py
@@ -0,0 +1,91 @@
+from db import Base, Scraper_Base
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.types import Text
+
+
+class Scraper(Scraper_Base):
+    __tablename__ = 'scraper'
+
+    id = Column(Integer, primary_key=True)
+    url = Column(String(200))
+    scraped_at = Column(DateTime)
+
+
+class Domain(Base):
+    __tablename__ = 'domain'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+
+
+class Organization(Base):
+    __tablename__ = 'organization'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    description = Column(Text)
+    domain_id = Column(Integer, ForeignKey('domain.id'))
+    headquarters_address = Column(String(200))
+    size = Column(String(100))
+    founded_at = Column(DateTime)
+    type = Column(String(100))
+    industry = Column(String(100))
+    revenue = Column(String(100))
+    competitors = Column(String(500))
+
+    domain = relationship(Domain)
+
+
+class Location(Base):
+    __tablename__ = 'location'
+
+    id = Column(Integer, primary_key=True)
+    country = Column(String(50))
+    state = Column(String(50))
+    city = Column(String(50))
+
+
+class Job(Base):
+    __tablename__ = 'job'
+
+    id = Column(Integer, primary_key=True)
+    source = Column(String(100))
+    title = Column(String(100))
+    created_at = Column(DateTime)
+    description = Column(Text)
+    organization_id = Column(Integer, ForeignKey('organization.id'))
+    location_id = Column(Integer, ForeignKey('location.id'))
+
+    organization = relationship(Organization)
+    location = relationship(Location)
+
+
+class AuthorJob(Base):
+    __tablename__ = 'author_job'
+
+    id = Column(Integer, primary_key=True)
+    job = Column(String(50))
+
+
+class AuthorLocation(Base):
+    __tablename__ = 'author_location'
+
+    id = Column(Integer, primary_key=True)
+    location = Column(String(80))
+
+
+class Review(Base):
+    __tablename__ = 'review'
+
+    id = Column(Integer, primary_key=True)
+    review_at = Column(DateTime)
+    summary = Column(String(100))
+    organization_id = Column(Integer, ForeignKey('organization.id'))
+    author_job_id = Column(Integer, ForeignKey('author_job.id'))
+    author_location_id = Column(Integer, ForeignKey('author_location.id'))
+    review = Column(Text)
+
+    organization = relationship(Organization)
+    author_job = relationship(AuthorJob)
+    author_location = relationship(AuthorLocation)
