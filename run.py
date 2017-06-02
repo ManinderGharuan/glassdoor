76cb4255c449ba5a2c024cc387a331d01f810e1e
Maninder Singh <maninderrai7@gmail.com>
2017-06-02 14:02:24 +0530

    Create main function which runs scraper and save information in database

---
+++ run.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from GlassdoorScraper import GlassdoorScraper
+from db import get_session
+from save_jobs import save_jobs_in_database
+
+
+def scrap():
+    """
+    Run scraper and save job_info in database
+    """
+    try:
+        session = get_session()
+        g = GlassdoorScraper()
+
+        for info in g.parse():
+            if info:
+                print("Got a job: ", info)
+                print("******" * 13)
+                save_jobs_in_database(session, info)
+    except Exception as error:
+        print('Exception in GlassdoorScraper: ', error)
+    finally:
+        session.close()
+
+
+if __name__ == "__main__":
+    scrap()
