a2311d68176ab71ba6a67b00aa7c4dc7680eec72
Maninder Singh <maninderrai7@gmail.com>
2017-06-06 18:13:53 +0530

    Add FreshersworldScraper to run scraper

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 from scrapers.GlassdoorScraper import GlassdoorScraper
+from scrapers.FreshersworldScraper import FreshersworldScraper
 from db import get_session
 from save_jobs import save_jobs_in_database
 
 
-def scrap():
+def run_scrapers():
     """
     Run scraper and save job_info in database
     """
@@ -20,9 +21,20 @@ def scrap():
                 save_jobs_in_database(session, info)
     except Exception as error:
         print('Exception in GlassdoorScraper: ', error)
+
+    try:
+        f = FreshersworldScraper()
+
+        for info in f.parse():
+            if info:
+                print("Got a job: ", info)
+                print("******" * 13)
+                save_jobs_in_database(session, info)
+    except Exception as error:
+        print("Exception in FreshersworldScraper: ", error)
     finally:
         session.close()
 
 
 if __name__ == "__main__":
-    scrap()
+    run_scrapers()
