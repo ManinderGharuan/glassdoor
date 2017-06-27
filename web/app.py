8ae4643055baebebdda3902e216d244bd7025f1d
Maninder Singh <maninderrai7@gmail.com>
2017-06-27 15:57:12 +0530

    Create routes
    - route signin
    - route register
    - route post
    - route logout

--- web/app.py
+++ web/app.py
@@ -1,11 +1,21 @@
-from flask import Flask, render_template, request
-from models import Organization
-from web.db import get_session
+from flask import Flask, render_template, request, redirect, flash
+from flask_login import LoginManager, login_user, logout_user, login_required
+from models import Organization, User
+from web.db import get_session, get_user_session
 from web.db.get_companies import get_companies
 
 app = Flask(__name__)
+app.secret_key = 'secure'
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 session = get_session()
+user_session = get_user_session()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user_session.query(User).filter(User.id == user_id).first()
 
 
 @app.route('/')
@@ -22,5 +32,66 @@ def show_mainpage():
 
     return render_template(
         'template.html', orgs=orgs, org=org,
-        current_page=current_page, pages=pages, search=search
-    )
+        current_page=current_page, pages=pages, search=search)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def user_register():
+    if request.method == "GET":
+        return render_template('signup.html')
+
+    query = user_session.query(User)
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if not username or not email or not password:
+        return redirect('/register')
+
+    if query.filter_by(email=email).first():
+        return redirect('/register')
+
+    user = User(username=username, email=email, password=password)
+    user_session.add(user)
+    user_session.commit()
+    login_user(user)
+
+    flash("User successfully registered")
+    return redirect('/')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def user_login():
+    if request.method == 'GET':
+        return render_template('signin.html')
+
+    query = user_session.query(User)
+    email = request.form.get('email')
+    password = request.form.get('password')
+    user = query.filter_by(email=email, password=password).first()
+
+    if user:
+        login_user(user)
+        flash('Logged in successfully.')
+        return redirect('/')
+
+    flash('Username or Password is invalid')
+    return redirect('/login')
+
+
+@app.route('/post')
+@login_required
+def post():
+    pass
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect('/')
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect('/login')
