eb3babbcc2dd7df22cb229805705df2c192beef3
Maninder Singh <maninderrai7@gmail.com>
2017-06-29 23:24:34 +0530

    Make post route working proper

--- web/app.py
+++ web/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request, redirect, flash
-from flask_login import LoginManager, login_user, logout_user, login_required
-from models import Organization, User
+from flask_login import (LoginManager, login_user, logout_user,
+                         login_required, current_user)
+from dateutil.parser import parse
+from models import User
 from web.db import get_session, get_user_session
 from web.db.get_companies import get_companies
+from scrapers.data import JobInfo
+from scrapers.save_jobs import save_jobs_in_database
 
 app = Flask(__name__)
 app.secret_key = 'secure'
@@ -25,14 +29,11 @@ def show_mainpage():
     id = int(request.args.get(
         'id')) if request.args.get('id') else request.args.get('id')
     search = request.args.get('keyword')
-    data = get_companies(session, Organization, current_page, id, search)
-    orgs = data.get('orgs')
-    org = data.get('org')
-    pages = data.get('pages')
+    data = get_companies(session, current_page, id, search)
 
     return render_template(
-        'template.html', orgs=orgs, org=org,
-        current_page=current_page, pages=pages, search=search)
+        'template.html', orgs=data.get('orgs'), org=data.get('org'),
+        current_page=current_page, pages=data.get('pages'), search=search)
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -45,10 +46,9 @@ def user_register():
     email = request.form.get('email')
     password = request.form.get('password')
 
-    if not username or not email or not password:
-        return redirect('/register')
-
-    if query.filter_by(email=email).first():
+    if not username or not email or not password or query \
+       .filter_by(email=email).first():
+        flash("Are you sure about information?")
         return redirect('/register')
 
     user = User(username=username, email=email, password=password)
@@ -73,25 +73,61 @@ def user_login():
     if user:
         login_user(user)
         flash('Logged in successfully.')
-        return redirect('/')
+        return redirect('/post')
 
     flash('Username or Password is invalid')
     return redirect('/login')
 
 
-@app.route('/post')
+@app.route('/post', methods=['GET', 'POST'])
 @login_required
 def post():
-    pass
+    if request.method == 'GET':
+        return render_template('postcompany.html')
+
+    session = get_session()
+    organization = request.form.get('post_company')
+    domain = request.form.get('domain')
+    org_desc = request.form.get('description')
+    headquarters_address = request.form.get('headquarters_address')
+    size = request.form.get('size')
+    org_type = request.form.get('type')
+    industry = request.form.get('industry')
+    revenue = request.form.get('revenue')
+    competitors = request.form.get('competitors')
+    phone_no = request.form.get('phone_number')
+    user_id = current_user.id
+
+    try:
+        founded_at = parse(request.form.get('founded_at'), fuzzy=True)
+    except ValueError:
+        founded_at = None
+
+    if not organization or not domain or not org_desc or not phone_no:
+        flash('Please fill every forms')
+        return redirect('/post')
+
+    org_fields = dict(organization=organization, org_desc=org_desc, size=size,
+                      founded_at=founded_at, phone_no=phone_no,
+                      headquarters_address=headquarters_address,
+                      org_type=org_type, revenue=revenue, org_domain=domain,
+                      competitors=competitors)
+    add_organization = JobInfo(org_fields=org_fields, city='Chandigarh',
+                               industries=industry, user_id=user_id)
+    save_jobs_in_database(session, add_organization)
+    flash("New company is added")
+    return redirect('/')
 
 
 @app.route('/logout')
 @login_required
 def logout():
+    flash("Successfully logout")
     logout_user()
     return redirect('/')
 
 
 @login_manager.unauthorized_handler
 def unauthorized():
+    flash("Only registerd user allow to add their company")
     return redirect('/login')
